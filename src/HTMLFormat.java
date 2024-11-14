7b1005997b66c0c020256982e33603368cfc618a
Jenny Wu <jennywu22553@gmail.com>
2024-11-14 10:39:56 -0600

    Added second variant of IFormat interface which is HTML format

---
+++ src/HTMLFormat.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class HTMLFormat implements IFormat {
+
+    public void format(ArrayList<String> fileContent) {
+        System.out.println("<html dir=\"ltr\" lang=\"en\">");
+        System.out.println("<head>");
+        for(int i = 0; i < fileContent.size(); i++){
+            System.out.println("<text=" + fileContent.get(i) + ">");
+        }
+        System.out.println("</head>");
+        System.out.println("</html>");
+    }
+}
