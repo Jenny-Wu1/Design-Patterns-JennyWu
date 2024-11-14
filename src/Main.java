e24558c28908e4e61e47f3c86a550dcf9f6c24e0
Jenny Wu <jennywu22553@gmail.com>
2024-11-14 10:26:56 -0600

    Added switch cases between formats

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> data = new ArrayList<>();
+        data.add("hwgugh");
+        data.add("eigwieg");
+        data.add("igjwgjgijgr");
+        textFile file = new textFile(data);
+
+        int option = 0;
+        switch (option) {
+            case 0:
+                file.printPlainText();
+                break;
+            case 1:
+                file.printHTML();
+                break;
+            case 2:
+                file.printMarkdown();
+                break;
+            default:
+                file.printPlainText();
+        }
+
+    }
 }
