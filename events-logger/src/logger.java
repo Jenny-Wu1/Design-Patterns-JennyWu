ce62c56da7e65bc5dc8713f7347048d9dab935d6
Jenny Wu <jennywu22553@gmail.com>
2024-11-24 11:05:22 -0600

    Implemented user story 1 by adding a single, centralized logger instance

--- events-logger/src/logger.java
+++ events-logger/src/logger.java
@@ -1,2 +1,15 @@
 public class logger {
+    private static logger uniqueInstance;
+
+    private logger () {}
+
+    public static logger getInstance() {
+        if (uniqueInstance == null) {
+            System.out.println("Creating unique instance of events logger");
+            uniqueInstance = new logger();
+        }
+        System.out.println("Returning instance of events logger");
+        return uniqueInstance;
+    }
+
 }
